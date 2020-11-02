b73ae3fff974aae69bc27e404fec8b782bce638a
Muhammad Meigooni <mmeigooni@gmail.com>
2020-11-02 07:50:49 -0700

    Fetches data, updates data with columns converting strings -> numbers, updates state with data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { csv } from 'd3';
 
-const Goldilocks = () => (
-  <div>
-    Juuust right!!
-  </div>
-)
+const csvPath = 'https://gist.githubusercontent.com/mmeigooni/4437b0b223c9812fbab5880f18444093/raw/planets-short.csv';
 
-ReactDOM.render(<Goldilocks />, document.getElementById('goldilocks'));
+const width = 960;
+const height = 2000;
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const row = d => {
+      d.Mass = +d.mass;
+      d.Radius = +d.radius;
+      d.Hzd = +d.hzd;
+      return d;
+    };
+
+    csv(csvPath, row)
+      .then(data => {
+        setData(data);
+      })
+      .catch(err => {
+        console.log('Error in parsing CSV');
+        console.log(err);
+      });
+  }, []);
+
+  if (!data) {
+    return <h1>Loading data...</h1>
+  }
+
+  return (
+    <h1>Got data!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// load data once using useEffect
+  // create new columns converting strings to numbers
+
+// Create xScale and yScale
+
+// plot the points
