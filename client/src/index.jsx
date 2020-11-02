aea5ca655a7dc4e75d886b0bc379065dfc5f83a9
Muhammad Meigooni <mmeigooni@gmail.com>
2020-11-02 08:57:48 -0700

    Creates basic axes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { csv, scaleLinear, min, max } from 'd3';
 
-const csvPath = 'https://gist.githubusercontent.com/mmeigooni/4437b0b223c9812fbab5880f18444093/raw/planets-short.csv';
+const csvPath = 'https://gist.githubusercontent.com/mmeigooni/0b968d836c4a3b7b0d0834a765481b10/raw/planets.csv';
 
 const width = 960;
 const height = 2000;
+const margin = { top: 20, right: 20, bottom: 20, left: 20};
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [ data, setData ] = useState(null);
+  const { top, right, bottom, left } = margin;
 
   useEffect(() => {
     const row = d => {
@@ -32,33 +34,57 @@ const App = () => {
     return <h1>Loading data...</h1>
   }
 
+  const innerHeight = height - top - bottom;
+  const innerWidth = width - left - right;
+
   const xScale = scaleLinear()
     .domain([min(data, d => d.Hzd), max(data, d => d.Hzd)])
-    .range([0, width]);
+    .range([0, innerWidth]);
 
   const yScale = scaleLinear()
     .domain([0, max(data, d => d.Mass)])
-    .range([0, height]);
+    .range([0, innerHeight]);
 
+  const rScale = scaleLinear()
+    .domain([0, max(data, d => d.Radius)])
+    .range([1, 15]);
+
+  console.log(xScale.ticks(2));
   return (
     <svg width={width} height={height}>
-      {data.map(d => (
-        <circle
-          cx={xScale(d.Hzd)}
-          cy={yScale(d.Mass)}
-          r={d.Radius * 10}
-        />
-      ))}
+      <g transform={`translate(${left + 20}, ${top})`}>
+        {xScale.ticks(2).map(tick => (
+          <g key={tick} transform={`translate(${xScale(tick)}, 0)`}>
+            <line y2={innerHeight} stroke="black"/>
+            <text style={{textAnchor: 'middle'}}>{tick}</text>
+          </g>
+        ))}
+        {yScale.ticks(6).map(tick => (
+          <g key={tick} transform={`translate(0, ${yScale(tick)})`}>
+            <line
+              x2={innerWidth}
+              stroke="black"
+            />
+            <text
+              style={{textAnchor: 'end'}}
+              dx="-5"
+              dy="5"
+            >
+              {tick}
+            </text>
+          </g>
+        ))}
+        {data.map(d => (
+          <circle
+            key={d.name}
+            cx={xScale(d.Hzd)}
+            cy={yScale(d.Mass)}
+            r={rScale(d.Radius)}
+          />
+        ))}
+      </g>
     </svg>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-// load data once using useEffect
-  // create new columns converting strings to numbers
-
-// Create xScale and yScale
-
-// plot the points
