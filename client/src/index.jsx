2cef480900f82968e839a44e5d62a10490dab8dc
Muhammad Meigooni <mmeigooni@gmail.com>
2020-11-05 17:59:36 -0700

    Replaces janky css with material-ui grid

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { csv, scaleLinear, min, max, scaleThreshold, scaleLog } from 'd3';
+import { Grid, Button } from '@material-ui/core';
 import { useData } from './helpers/useData';
-import { ChartContainer } from './components/ChartContainer.jsx'
-import { AxisTop } from './components/AxisTop.jsx'
-import { AxisLeft } from './components/AxisLeft.jsx'
-import { Planets } from './components/Planets.jsx'
+import { ChartContainer } from './components/ChartContainer.jsx';
+import { AxisTop } from './components/AxisTop.jsx';
+import { AxisLeft } from './components/AxisLeft.jsx';
+import { Planets } from './components/Planets.jsx';
+
+const textLeft = {
+  textAlign: 'left',
+};
+
+const width100 = {
+  width: '100%',
+};
+
+const top30bottom60 = {
+  textAlign: 'center',
+  marginTop: '30px',
+  marginBottom: '60px',
+};
 
 const width = 960;
 const height = 2000;
@@ -55,23 +70,55 @@ const App = () => {
 
   return (
     <>
-      <button type="button" id="log" onClick={() => {setScale('log')}}>Logarithmic</button>
-      <button type="button" id="linear" onClick={() => {setScale('linear')}}>Linear</button>
-      <ChartContainer width={width} height={height} left={left} top={top} >
-        <AxisTop innerWidth={innerWidth} innerHeight={innerHeight} />
-        <AxisLeft yScale={yScale} innerWidth={innerWidth} />
-        <Planets
-          data={data}
-          xScale={xScale}
-          yScale={yScale}
-          rScale={rScale}
-          colorScale={colorScale}
-          xValue={xValue}
-          yValue={yValue}
-          rValue={rValue}
-          nameValue={nameValue}
-        />
-      </ChartContainer>
+      <Grid justify="center" container>
+        <Grid item md={10} xs>
+          <h1>GOLDILOCKS WORLDS: JUST RIGHT FOR LIFE?</h1>
+        </Grid>
+        <Grid item md={4} xs style={textLeft}>
+          <h3>Of the 1,780 confirmed planets beyond our solar system, as many as 16 are located in their star’s habitable zone, where conditions are neither too hot nor too cold to support life. Size also matters: A planet that’s too small can’t maintain an atmosphere; one that’s too large will have a crushing atmosphere. A recently detected planet 493 light-years from Earth, Kepler-186f, is close to Earth's size and is located in its solar system's habitable zone.</h3>
+        </Grid>
+        <Grid item md={2} xs>
+          <img style={width100} src="https://i.imgur.com/P462pRe.png" alt="zones"></img>
+        </Grid>
+      </Grid>
+      <Grid justify="center" container style={top30bottom60}>
+        <Grid item md={2}>
+          <Button variant="contained" color="primary" id="log" onClick={() => {setScale('log')}}>Logarithmic</Button>
+        </Grid>
+        <Grid item md={2}>
+          <Button variant="contained" color="primary" id="linear" onClick={() => {setScale('linear')}}>Linear</Button>
+        </Grid>
+      </Grid>
+      <Grid justify="center" container>
+        <Grid item md={2}>
+          Too cold
+        </Grid>
+        <Grid item md={2}>
+          Just right
+        </Grid>
+        <Grid item md={2}>
+          Too warm
+        </Grid>
+      </Grid>
+      <Grid justify="center" container>
+        <Grid item md={10}>
+          <ChartContainer width={width} height={height} left={left} top={top} >
+            <AxisTop innerWidth={innerWidth} innerHeight={innerHeight} />
+            <AxisLeft yScale={yScale} innerWidth={innerWidth} />
+            <Planets
+              data={data}
+              xScale={xScale}
+              yScale={yScale}
+              rScale={rScale}
+              colorScale={colorScale}
+              xValue={xValue}
+              yValue={yValue}
+              rValue={rValue}
+              nameValue={nameValue}
+            />
+          </ChartContainer>
+        </Grid>
+      </Grid>
     </>
   )
 }
